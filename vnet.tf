67293188be7922e68bc5c7ec2b94bdc693063efa
Kali <kalimuthuc@yahoo.com>
2021-06-07 18:08:41 +0000

    added vnet module patch

--- vnet.tf
+++ vnet.tf
@@ -1,12 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "eastus"
-}
-
 module "network" {
   source              = "Azure/network/azurerm"
   resource_group_name = "${var.prefix}-workshop"
